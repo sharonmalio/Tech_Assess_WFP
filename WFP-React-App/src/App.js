6d50b1fca50ed14eca5bf8c59b2e222e4e31eeb0
sharonmalio <kaninimalio@gmail.com>
2019-12-17 04:20:40 -0500

    working on the table format

--- WFP-React-App/src/App.js
+++ WFP-React-App/src/App.js
@@ -9,7 +9,7 @@ function App () {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="tag">Welcome to WHO to WFP Life Expectancy data</p>
+        <p className="tag">Welcome to WHO-WFP Life Expectancy data</p>
         <Link to="/life-expectancy"> Life Expectancy data </Link>
       </header>
     </div>
